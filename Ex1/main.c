1e157a71fff1ce0f6c0282faf9808b177809fcc8
talia <talias1235@gmail.com>
2021-10-31 15:28:39 +0200

    finish

--- Ex1/main.c
+++ Ex1/main.c
@@ -6,30 +6,34 @@ int main(){
     printf("Enter start and end\n");
     scanf("%d %d",&start,&end);
     int i;
-    printf("primes: ");
-    for(i=start; i<=end; i++){
-        if(isPrime(i)){
-            printf("%d  ", i);
-        }
-    }
-    printf("\n");
-    printf("armstrongs: ");
+    
+    printf("The Armstrong numbers are: ");
     for(i=start; i<=end; i++){
         if(isArmstrong(i)==1){
             printf("%d  ", i);
         }
     }
+
     printf("\n");
-    printf("strongs: ");
+    printf("The Palindromes are: ");
     for(i=start; i<=end; i++){
-        if(isStrong(i)==1){
+        if(isPalindrome(i)==1){
             printf("%d  ", i);
         }
     }
+  
     printf("\n");
-    printf("palindroms: ");
+    printf("The Prime numbers are: ");
     for(i=start; i<=end; i++){
-        if(isPalindrome(i)==1){
+        if(isPrime(i)){
+            printf("%d  ", i);
+        }
+    }
+    
+    printf("\n");
+    printf("The Strong numbers are: ");
+    for(i=start; i<=end; i++){
+        if(isStrong(i)==1){
             printf("%d  ", i);
         }
     }
