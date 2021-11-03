b8da1eac515a1f37701da8eeccfbe88ea52421af
talia <talias1235@gmail.com>
2021-11-03 15:10:36 +0200

    main

--- main.c
+++ main.c
@@ -4,31 +4,29 @@
 int main(){
     int start, end;
     scanf("%d %d",&start,&end);
-    int i;
-    
     printf("The Armstrong numbers are:");
-    for(i=start; i<=end; i++){
+    for(int i=start; i<=end; i++){
         if(isArmstrong(i)==1){
             printf(" %d", i);
         }
     }
     printf("\n");
     printf("The Palindromes are:");
-    for(i=start; i<=end; i++){
+    for(int i=start; i<=end; i++){
         if(isPalindrome(i)==1){
             printf(" %d", i);
         }
     }
     printf("\n");
     printf("The Prime numbers are:");
-    for(i=start; i<=end; i++){
-        if(isPrime(i)){
+    for(int i=start; i<=end; i++){
+        if(isPrime(i)==1){
             printf(" %d", i);
         }
     }
     printf("\n");
     printf("The Strong numbers are:");
-    for(i=start; i<=end; i++){
+    for(int i=start; i<=end; i++){
         if(isStrong(i)==1){
             printf(" %d", i);
         }
