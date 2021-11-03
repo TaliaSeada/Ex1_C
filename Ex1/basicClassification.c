7c5850a4d1133bc37854eb8c84e2ae384a1226e0
talia <talias1235@gmail.com>
2021-11-03 09:59:20 +0200

    prime fixed

--- Ex1/basicClassification.c
+++ Ex1/basicClassification.c
@@ -25,10 +25,13 @@ int isStrong(int num){
     
 }
 int isPrime(int num){
-    if(num < 1) {
+    if(num == 0) {
         return 0;
     }
-    for(int i = 1; i <= num/2; i++){
+    if(num == 1) {
+        return 1;
+    }
+    for(int i = 2; i <= num/2; i++){
         if(num % i == 0){
             return 0;
         }
